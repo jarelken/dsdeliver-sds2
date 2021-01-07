939f0eb12c5be80fd780b9ff6d965caa15d4bdc4
unknown <lelo_taguti@hotmail.com>
2021-01-07 01:35:20 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -31,6 +31,7 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 		
 	}
+	
 	@Transactional
 	public OrderDTO insert(OrderDTO dto){
 		Order order = new Order(null, dto.getAddress(), dto.getLatitude(),
@@ -39,6 +40,14 @@ public class OrderService {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
 		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id){
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
 		order = repository.save(order);
 		return new OrderDTO(order);
 	}
