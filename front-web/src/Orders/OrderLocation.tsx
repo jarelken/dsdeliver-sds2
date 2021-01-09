69f9b5ea11d398a7a004459c1df09b6f7f40802e
unknown <lelo_taguti@hotmail.com>
2021-01-08 22:05:49 -0300

    Finish front-web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
-import { OrderLocationdata } from './types';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
     lat: -23.2953872,
@@ -20,7 +20,7 @@ type Place ={
 
 
 type Props ={
-    onChangeLocation: (location: OrderLocationdata)=> void;
+    onChangeLocation: (location: OrderLocationData)=> void;
 }
 function OrderLocation({onChangeLocation}: Props) {
     const [address, setAddress] = useState<Place>({
@@ -71,7 +71,7 @@ function OrderLocation({onChangeLocation}: Props) {
                 center={address.position}
                 zoom={13}
                 key={address.position.lat}
-                scrollWheelZoom>
+                scrollWheelZoom={false}>
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
