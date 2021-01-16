6aa3e8838db2200412183765674988014c524177
unknown <lelo_taguti@hotmail.com>
2021-01-16 18:07:53 -0300

    Add Orders List

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import  Header  from '../Header';
 
 
 function Home() {
 
+  const navigation = useNavigation();
+
   const handleOnPress = ()=>{
-      
+    navigation.navigate('Orders');
   } 
   return (
     <>
+    <Header/>
     <View style={styles.container}>
       <Image source={require('../assets/deliveryman.png')} />
       <Text style={styles.title}>Acompanhe os pedidos e entregue no prazo</Text>
@@ -17,7 +22,7 @@ function Home() {
       restaurante na palma da sua mão</Text>
     </View>
     <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={}>
+        <RectButton style={styles.button} onPress={handleOnPress}>
             <Text style={styles.buttonText}>VER PEDIDOS</Text>
         </RectButton>
     </View>
